fee3147b53d292a6cc35a00ef154423d8032436d
chiranjeevi <chiranjeevi@172.16.1.47>
2017-08-31 10:07:33 +0530

    Reverse a Stack using Recursion

---
+++ src/algorithms/stack/ReverseStackUsingRecursion.java
@@ -0,0 +1,33 @@
+package algorithms.stack;
+
+import java.util.Iterator;
+import java.util.Stack;
+
+public class ReverseStackUsingRecursion {
+	public static void main(String[] args) {
+		Stack<Integer> stack = new Stack<>();
+		stack.push(10);
+		stack.push(20);
+		stack.push(30);
+		stack.push(40);
+		stack.push(50);
+		System.out.println("Stack :");
+		Iterator<Integer> iterator = stack.iterator();
+		while (iterator.hasNext()) {
+			System.out.print(iterator.next() + ",");
+		}
+		System.out.println("\nReverse : ");
+		ReverseStackUsingRecursion.reverse(stack);
+	}
+
+	public static void reverse(Stack<Integer> stack) {
+		if (stack.empty()) {
+			return;
+		}
+		int next = stack.pop();
+		System.out.print(next + ",");
+		reverse(stack);
+
+
+	}
+}
